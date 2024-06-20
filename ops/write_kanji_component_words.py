ff1c5899c45ec9eb0adcab4e0dab281d9f25dabb
jhhr <jormki@gmail.com>
2024-06-20 19:21:42 +0300

    Use functional file endings instead of .log

--- ops/write_kanji_component_words.py
+++ ops/write_kanji_component_words.py
@@ -16,8 +16,8 @@ from ..utils import get_field_config
 
 DEBUG = False
 
-KANJI_STORIES_LOG = 'kanji_stories.log'
-KANJI_STORY_COMPONENT_WORDS_LOG = 'kanji_story_component_words.log'
+KANJI_STORIES_LOG = 'kanji_stories.txt'
+KANJI_STORY_COMPONENT_WORDS_LOG = 'kanji_story_component_words.json'
 
 
 def write_components_to_file(
