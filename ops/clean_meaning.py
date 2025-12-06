6466c4e814c40f51e457c8660d9fcd544c0ee8f6
jhhr <jormki@gmail.com>
2025-12-06 16:11:06 +0200

    Refactor meaning extraction to handle multiple sentences and improve prompt clarity

--- ops/clean_meaning.py
+++ ops/clean_meaning.py
@@ -20,16 +20,22 @@ DEBUG = False
 def get_single_meaning_from_model(
     config: Dict[str, str],
     vocab: str,
-    sentence: str,
+    sentences: list[str],
     jp_dict_entry: str,
     en_dict_entry: str = "",
 ):
     jp_meaning_return_field = "cleaned_meaning"
     en_meaning_return_field = "english_meaning"
-    prompt = f"""Below, the dictionary entry for the word or phrase may contain multiple meanings. Your task is to extract the one meaning matching the usage of the word in the sentence.
+    sentences_formatted = ""
+    if len(sentences) > 1:
+        for sen in sentences:
+            sentences_formatted += f"- {sen}\n"
+    else:
+        sentences_formatted = sentences[0]
+    prompt = f"""Below, the dictionary entry for the word or phrase may contain multiple meanings. Your task is to either 1) extract the one meaning 2) or combine and rephrase meanings matching the usage of the word in the sentence{'s' if len(sentences) > 1 else ''}.
 - If there are only two meanings for this word or phrase, one literal and one figurative, pick both and shorten their respective descriptions.
-- Omit any example sentences the matching meaning included (often include within 「」 brackets).
-- Shorten and simplify the meaning as much possible, ideally into 1-3 sentences, with more complex meanings being allowed more sentences.
+- Omit any example sentences the matching meaning(s) included (often included within 「」 brackets).
+- Shorten and simplify the meaning as much possible, ideally into 1 sentence and at most 3, with more complex meanings being allowed more sentences.
 - Descriptions of animals and plants are often scientific. From these omit descriptions on their ecology and only describe their appearance and type of plant/animal with simple language.
 - In case there is only a single meaning, return that.
 - Clean off meaning numberings and other notation leaving only a plain text description.
@@ -42,7 +48,7 @@ Return the possibly modified English meaning as the value of the key "{en_meanin
 
 Word or phrase: {vocab}
 ---
-Sentence: {sentence}
+Sentence{'s' if len(sentences) > 1 else ''}: {sentences_formatted}
 ---
 Japanese dictionary entry: {jp_dict_entry}
 """
@@ -62,16 +68,23 @@ Japanese dictionary entry: {jp_dict_entry}
 def get_new_meaning_from_model(
     config: Dict[str, str],
     vocab: str,
-    sentence: str,
+    sentences: list[str],
 ) -> tuple[str, str]:
     jp_meaning_return_field = "new_meaning"
     en_meaning_return_field = "english_meaning"
-    prompt = f"""Below is a sentence containing a word or phrase. Your task is to generate a short monolingual dictionary style definition of the general meaning used in the sentence by the word or phrase.
+    sentences_formatted = ""
+    if len(sentences) > 1:
+        for sen in sentences:
+            sentences_formatted += f"- {sen}\n"
+    else:
+        sentences_formatted = sentences[0]
+    prompt = f"""Below {'is a sentence' if len(sentences) == 1 else 'are sentences each'} containing a certain word or phrase. Your task is to generate a short monolingual dictionary style definition of the general meaning used in the sentence by the word or phrase.
 
+- Generally aim to for the definition to be a single sentence. If it is necessary to explain more, the maximum length should be 3 sentences.
+- Do not overfit the definition to the sentence{'s' if len(sentences) > 1 else ''}, but rather aim for a general definition that fits the usage in {'each sentence' if len(sentences) > 1 else 'the sentence'}.
 - If there are two usage patterns for this word or phrase - for example, one literal and one figurative - describe both shortly.
 - If there are more than two usage patterns for this word or phrase, describe the one used in the sentence.
 - The word itself should not be used in the definition.
-- Generally aim to for the definition to be a single sentence. If it is necessary to explain more, the maximum length should be 3 sentences.
 
 The definition should be in the same language as the sentence. Also, generate a very short English translation of the meaning, ideally a list of equivalent words or phrases but explaining further, if necessary.
 
@@ -80,7 +93,7 @@ Return the meaning ias the value of the key "{jp_meaning_return_field}".
 Return the English translation as the value of the key "{en_meaning_return_field}".
 
 Word or phrase: {vocab}
-Sentence: {sentence}
+Sentence{'s' if len(sentences) > 1 else ''}: {sentences_formatted}
 """
     model = config.get("word_meaning_model", "")
     result = get_response(model, prompt)
@@ -117,6 +130,7 @@ def clean_meaning_in_note(
         english_meaning_field = get_field_config(config, "english_meaning_field", note_type)
         word_field = get_field_config(config, "word_field", note_type)
         sentence_field = get_field_config(config, "sentence_field", note_type)
+        word_list_field = get_field_config(config, "word_list_field", note_type)
     except KeyError as e:
         print(e)
         return False
@@ -127,6 +141,14 @@ def clean_meaning_in_note(
         print("english_meaning_field in note", english_meaning_field in note)
         print("word_field in note", word_field in note)
         print("sentence_field in note", sentence_field in note)
+
+    # Find other notes with the same word
+    other_note_ids = mw.col.find_notes(f'"{word_list_field}:*{note.id}*" -nid:{note.id}')
+    other_sentences = []
+    for onid in other_note_ids:
+        onote = mw.col.get_note(onid)
+        if sentence_field in onote and onote[sentence_field] not in other_sentences:
+            other_sentences.append(onote[sentence_field])
     # Check if the note has the required fields
     if (
         meaning_field in note
@@ -140,12 +162,12 @@ def clean_meaning_in_note(
         jp_dict_entry = note[meaning_field]
         en_dict_entry = note[english_meaning_field]
         word = note[word_field]
-        sentence = note[sentence_field]
+        sentences = [note[sentence_field]] + other_sentences
         # Check if the value is non-empty
         if jp_dict_entry:
             # Call API to get single meaning from the raw dictionary entry
             new_jp_meaning, new_en_meaning = get_single_meaning_from_model(
-                config, word, sentence, jp_dict_entry, en_dict_entry
+                config, word, sentences, jp_dict_entry, en_dict_entry
             )
 
             # Update the note with the new value
@@ -157,7 +179,7 @@ def clean_meaning_in_note(
             return False
         else:
             # If there's no dict_entry, we'll use chatGPT to generate one from scratch
-            new_meaning, en_meaning = get_new_meaning_from_model(config, word, sentence)
+            new_meaning, en_meaning = get_new_meaning_from_model(config, word, sentences)
             note[meaning_field] = new_meaning
             note[english_meaning_field] = en_meaning
             if new_meaning != "" or en_meaning != "":
