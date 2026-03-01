31808726251df181229a927e1a65bad8890a5a90
jhhr <jormki@gmail.com>
2026-03-01 15:23:54 +0200

    Use component words in kanji story examples, add more examples

--- async_api_ops/make_kanji_story.py
+++ async_api_ops/make_kanji_story.py
@@ -1,24 +1,33 @@
 import json
 import logging
-from anki.notes import Note, NoteId
-from anki.collection import Collection
-from aqt import mw
-from aqt.browser import Browser
-from aqt.utils import showWarning
 from collections.abc import Sequence
 from pathlib import Path
 
-from ..configuration import KANJI_STORY_COMPONENT_WORDS_LOG
+from anki.collection import Collection
+from anki.notes import Note, NoteId
+from aqt import mw
+from aqt.browser import Browser
+from aqt.utils import showWarning
 
-from .base_ops import (
-    get_response,
-    bulk_notes_op,
-    selected_notes_op,
-    AsyncTaskProgressUpdater,
-)
+from ..configuration import KANJI_STORY_COMPONENT_WORDS_LOG
 from ..utils import get_field_config
+from .base_ops import (
+    AsyncTaskProgressUpdater,
+    bulk_notes_op,
+    get_response,
+    selected_notes_op,
+)
 
 logger = logging.getLogger(__name__)
+
+
+def get_component_words_section(
+    components: list[str], components_dict: dict[str, str]
+) -> str:
+    component_words = ", ".join(
+        [components_dict.get(component, component) for component in components]
+    )
+    return f"  component_radicals_or_kanji: {components}\n  words_to_use_in_story_for_components: {component_words}"
 
 
 def get_kanji_story_from_model(
@@ -29,25 +38,20 @@ def get_kanji_story_from_model(
 ) -> str:
     media_path = Path(mw.pm.profileFolder(), "collection.media")
     # Get stored dict of words used for component in the kanji_story_component_words.log file
-    with open(Path(media_path, KANJI_STORY_COMPONENT_WORDS_LOG), "r", encoding="utf-8") as f:
+    with open(
+        Path(media_path, KANJI_STORY_COMPONENT_WORDS_LOG), "r", encoding="utf-8"
+    ) as f:
         try:
             component_words_dict = json.loads(f.read())
         except json.JSONDecodeError as e:
             print(f"Error reading component words dict: {e}")
             component_words_dict = {}
 
-    component_words = []
-    # Get the words for each component
-    for component in components.split(","):
-        if component in component_words_dict:
-            component_words.append(component_words_dict[component])
-
     return_field = "new_story"
 
     prompt = (
         f"kanji: {kanji}"
-        f"\ncomponent_radicals_or_kanji: {components}"
-        f"\nwords_to_use_in_story_for_components: {component_words}"
+        f"\n{get_component_words_section(components.split(','), component_words_dict)}"
     )
     if current_story:
         prompt += f"\ncurrent_story_in_japanese: {current_story}"
@@ -82,42 +86,50 @@ def get_kanji_story_from_model(
         "\n"
         "\nExamples of stories for other kanji:"
         "\n  kanji: 裾"
-        "\n  component_radicals_or_kanji: 衤,居"
+        f"\n{get_component_words_section(['衤', '居'], component_words_dict)}"
         "\n  story: <i>ころも</i> の なか に <i>いる</i> と、 ぬけた <b>すそ</b> が ひろがる。"
         "\n"
         "\n  kanji: 熱"
-        "\n  component_radicals_or_kanji: 埶,灬"
+        f"\n{get_component_words_section(['廾', '灬'], component_words_dict)}"
         "\n  story: <i>どろだんご</i> が <i>れっか</i>したら、たかい <b>ねつ</b> が できる"
         "\n"
         "\n  kanji: 柳"
-        "\n  component_radicals_or_kanji: 木,卯"
+        f"\n{get_component_words_section(['木', '卯'], component_words_dict)}"
         "\n  story: <i>き</i> が <i>うさぎの みみ</i> の ように しなやか、<b>やなぎ</b>"
         "\n"
         "\n  kanji: 捩"
-        "\n  component_radicals_or_kanji: 扌,戻"
+        f"\n{get_component_words_section(['扌', '戻'], component_words_dict)}"
         "\n  story: <i>て</i> が <i>もどせない</i>、そんなに <b>よじっている</b>。"
         "\n"
         "\n  kanji: 移"
-        "\n  component_radicals_or_kanji: 禾,多"
+        f"\n{get_component_words_section(['禾', '多'], component_words_dict)}"
         "\n  story: <i>のぎ</i> が <i>おおくて</i>、それ を くら に <b>うつして</b>みましょう。"
         "\n"
         "\n  kanji: 侶"
-        "\n  component_radicals_or_kanji: 亻,呂"
+        f"\n{get_component_words_section(['亻', '呂'], component_words_dict)}"
         "\n  story: <i>ひと</i> の <i>せぼね</i> は あばらぼね の はん<b>りょ</b> だ。"
         "\n"
         "\n  kanji: 宮"
-        "\n  component_radicals_or_kanji: 宀,呂"
+        f"\n{get_component_words_section(['宀', '呂'], component_words_dict)}"
         "\n  story: <i>したぎ かんむり</i> の したに <i>せぼね</i> の ような はしら が きゅうでんの"
         " いりぐちに たった"
         "\n"
         "\n  kanji: 蹴"
-        "\n  component_radicals_or_kanji: 足,就"
+        f"\n{get_component_words_section(['足', '就'], component_words_dict)}"
         "\n  story: <i>あし</i> の <i>しゅうしょく</i> は もの を <b>ける</b> こと。"
         "\n"
         "\n  kanji: 諭"
-        "\n  component_radicals_or_kanji: 言,俞"
+        f"\n{get_component_words_section(['言', '俞'], component_words_dict)}"
         "\n  story: <i>いいたい</i> こと を <i>いやしの こぶね</i> を こぎ ながら つたえる と、"
         "せんちょう が しずに してくれと <b>さとした</b>"
+        "\n"
+        "\n kanji: 嘘"
+        f"\n{get_component_words_section(['口', '虚'], component_words_dict)}"
+        "\n  story: <i>くち</i> から でる <i>むなしい</i> <b>うそ</b>..."
+        "\n"
+        "\n kanji: 勇"
+        f"\n{get_component_words_section(['マ', '男'], component_words_dict)}"
+        "\n  story: <i>ま！</i> <i>おとこらしい！</i> <b>いさましい</b> <b>ゆう</b>き を もっている ね"
         "\n"
         f'\nReturn the new story in a JSON string as the value of the key "{return_field}".'
     )
@@ -164,7 +176,9 @@ def make_story_for_note(
         current_story = note[story_field]
         # Check if the value is non-empty
         if components:
-            new_story = get_kanji_story_from_model(config, kanji, components, current_story)
+            new_story = get_kanji_story_from_model(
+                config, kanji, components, current_story
+            )
 
             # Update the note with the new value
             note[story_field] = new_story
@@ -211,7 +225,9 @@ def bulk_make_stories_op(
 
 
 def make_stories_for_selected_notes(nids: Sequence[NoteId], parent: Browser):
-    progress_updater = AsyncTaskProgressUpdater(title="Async AI op: Making kanji stories")
+    progress_updater = AsyncTaskProgressUpdater(
+        title="Async AI op: Making kanji stories"
+    )
     done_text = "Updated stories"
     bulk_op = bulk_make_stories_op
     return selected_notes_op(done_text, bulk_op, nids, parent, progress_updater)
